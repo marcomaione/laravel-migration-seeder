470b265df768024df12cd328fcc2f280197bca84
marco maione <maionemarco14@gmail.com>
2022-03-31 16:26:57 +0200

    aggiornamento

---
+++ database/seeds/travelsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class travelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
